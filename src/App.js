b10ed1e999f6d5b76ba9fd0b0035713518086bd9
k-tuerk <ktuerk.4881@gmail.com>
2021-01-16 14:33:50 -0500

    add sparklines and update css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Weather from "./Weather";
 import Search from "./Search";
 import Forecast from "./Forecast";
@@ -12,27 +10,36 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 
+import { Sparklines, SparklinesLine, SparklinesSpots }from 'react-sparklines'
+
 export default function App() {
   return (
     <div className="App">
       <Container>
         <Row>
           <Col className="col-8 my-auto today">
-            <div id="world">
+            {/* <div id="world">
               <img src="/world_map_.svg" alt="weatherImage" id="world" />
-            </div>
+            </div> */}
             <h1>Toronto</h1>
             <Weather />
-            <Search />
-            <Date />
           </Col>
           <Col className="col-4 my-auto" id="forecast">
             <Card className="card mx-auto">
               <Forecast />
             </Card>
           </Col>
+          <Col>
+          <Sparklines data={[5, 10, 5, 20, 8, 15]} limit={5} width={100} height={20} margin={5}>
+      <SparklinesLine color="#3f72af" />
+      <SparklinesSpots style={{ fill: "#3f72af" }}/>
+      </Sparklines>
+      <Search />
+            <Date />
+          </Col>
         </Row>
       </Container>
+  
       <div>
         <p className="coded-by">
           <a href="https://github.com/k-tuerk/weather-app-react">Open-source code</a>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+// import { Sparklines } from 'react-sparklines'
 
 
 
